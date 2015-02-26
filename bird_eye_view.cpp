e95055e527f911000f26fc26e455da2c41b79680
wangzhen <zwang0725@gmail.com>
2015-02-26 22:06:43 +0800

    2.26

--- bird_eye_view.cpp
+++ bird_eye_view.cpp
@@ -29,8 +29,64 @@ void calc_virtual_cam()
 	K_inv = K.inv();
 }
 
-void distort()
+void distort(Mat &K,Mat &D,Mat &R,Mat &P,Size size,Mat &map1,Mat &map2)
 {
+	map1.create(size,CV_32FC1);
+	map2.create(size,CV_32FC1);
+
+    cv::Vec2d f, c;
+
+    Matx33d camMat = K;
+    f = Vec2d(camMat(0, 0), camMat(1, 1));
+    c = Vec2d(camMat(0, 2), camMat(1, 2));
+
+    Vec4d k = Vec4d::all(0);
+	Matx33d dis_coff = D;
+	k = Vec4d(dis_coff(0,0),dis_coff(0,1),dis_coff(0,2),dis_coff(0,3));
+
+    cv::Matx33d RR  = cv::Matx33d::eye();
+
+    R.convertTo(RR, CV_64F);
+
+    cv::Matx33d PP = cv::Matx33d::eye();
+   
+  P.colRange(0, 3).convertTo(PP, CV_64F);
+
+    cv::Matx33d iR = (PP * RR).inv(cv::DECOMP_SVD);
+
+    for( int i = 0; i < size.height; ++i)
+    {
+        float* m1f = map1.ptr<float>(i);
+        float* m2f = map2.ptr<float>(i);
+        short*  m1 = (short*)m1f;
+        ushort* m2 = (ushort*)m2f;
+
+        double _x = i*iR(0, 1) + iR(0, 2),
+               _y = i*iR(1, 1) + iR(1, 2),
+               _w = i*iR(2, 1) + iR(2, 2);
+
+        for( int j = 0; j < size.width; ++j)
+        {
+            double x = _x/_w, y = _y/_w;
+
+            double r = sqrt(x*x + y*y);
+            double theta = atan(r);
+
+            double theta2 = theta*theta, theta4 = theta2*theta2, theta6 = theta4*theta2, theta8 = theta4*theta4;
+            double theta_d = theta * (1 + k[0]*theta2 + k[1]*theta4 + k[2]*theta6 + k[3]*theta8);
+
+            double scale = (r == 0) ? 1.0 : theta_d / r;
+            double u = f[0]*x*scale + c[0];
+            double v = f[1]*y*scale + c[1];
+
+            m1f[j] = (float)u;
+            m2f[j] = (float)v;
+
+            _x += iR(0, 0);
+            _y += iR(1, 0);
+            _w += iR(2, 0);
+        }
+    }
 }
 
 int main(int argc,char *argv[])
